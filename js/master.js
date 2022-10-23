27e7a948da6822eae933c87f075bba0ee84e0b36
Mohammedfouadd <105463586+Mohammedfouadd@users.noreply.github.com>
2022-10-23 17:13:59 +0200

    frist animate

---
+++ js/master.js
@@ -0,0 +1,8 @@
+let loding = document.querySelector('.loding');
+let padge = document.querySelector('.padge');
+
+setInterval(() => {
+
+loding.style.display = "none"
+padge.style.display = "block"
+},10000)
